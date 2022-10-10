d13903bec4c07d019c1ee7afab14315040054eb2
Rafael Paulo <rafael8paulo@gmail.com>
2022-10-09 21:36:46 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -0,0 +1,59 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+@Entity
+@Table(name = "tab_category")
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto incremento
+    private Long Id;
+    private String name;
+
+    public Category(Long id, String name) {
+        Id = id;
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(Id, category.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
